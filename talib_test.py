3c2f66db47b51822769bf11321841ae293771112
Vladimir Pedchenko <paralogyX@gmail.com>
2024-06-17 18:46:41 +0000

    quick ta-lib test
    Downgrade main env and reinstall ta-lib, maybe via conda. But read git first!!!

---
+++ talib_test.py
@@ -0,0 +1,9 @@
+import talib as ta
+import yfinance as yf
+
+
+msft = yf.Ticker("AAPL")
+aapl = yf.download('AAPL', '2019-1-1','2019-12-27')
+aapl['Simple MA'] = ta.SMA(aapl['Close'],14)
+aapl['EMA'] = ta.EMA(aapl['Close'], timeperiod = 14)
+print(aapl.tail())
